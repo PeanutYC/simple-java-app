382da1594e6db611f065c8ded17e5d1346ec978b
MainUser <72436355+PeanutYC@users.noreply.github.com>
2026-04-06 15:22:11 +0800

    My second change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("modified from local project.");
+        System.out.println("Another change in my local project.");
     }
 
     public String getMessage() {
